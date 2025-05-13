9c5b23b88a4fd694b0161b8296ebc59c2d0123ac
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-13 15:27:38 +0530

    Create to print addition of 2 num .c

---
+++ to print addition of 2 num .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int add(int a,int b)
+{
+    int c=a+b;
+    return 0;
+}
+int main()
+{
+    int result=add(10,20);
+    printf("result is:%d",result);
+    return 0;
+}
+
+    
