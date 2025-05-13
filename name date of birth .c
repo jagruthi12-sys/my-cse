cb5dbe0e31587b6a5d36885c83c1657f201f36b0
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-13 15:45:38 +0530

    Create name date of birth .c

---
+++ name date of birth .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int shridevi(int a,int b)
+{
+    printf("jagruthi");
+    printf("date of birth");
+    printf("age\n");
+    printf("mobile number\n");
+}
+int main()
+{
+    shridevi(5,7);
+    return 0;
+}
