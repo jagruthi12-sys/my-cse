f0d884b8e00ea3117d0a4e2736c4a8f21dc1005a
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-14 10:08:01 +0530

    Create pass fail .c

---
+++ pass fail .c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main() {
+    int pass=1;
+    if(pass)
+    {
+        printf("fail in the exam.\n");
+    }
+    else
+    {
+        printf("pass in the exam.\n");
+    }
+    return 0;
+}
+    
+    
