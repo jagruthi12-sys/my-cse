cdd64ec3a6ebb068979b2af0e0c875248588b379
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-14 15:43:33 +0530

    Create factorial   pro  .c

---
+++ factorial   pro  .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n,c;
+    printf("enter the elements n\n");
+    scanf("%d",&n);
+    if (n==0);
+    printf("1");
+    }
+    else{
+        for(i=1;i<=n;i++){
+            c *=1;
+        }
+             printf("%d",c);
+        }
+}
+
+    
