bd77afea5024a5d7f21bc26531fafc065e7b075a
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-14 11:46:09 +0530

    Create 10 to 100 .c

---
+++ 10 to 100 .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int num;
+int main() {
+    scanf("%d",&num);
+    if(num>=10&&num<=100)
+    {
+        printf("number present between 10 and 100");
+    }
+    else
+    {
+        printf("number is not present between 10 and 100");
+    }
+    return 0;
+}
+    
+    
+
