d26041c37e7b65a32a115b1264e53004bd206aa2
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-15 12:24:44 +0530

    pointer decrement  .c

---
+++ pointer decrement  .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+    int arr[]={10,20,30,40,50};
+    int *ptr = arr;
+    printf("intial value:%d\n",*ptr);
+    ptr--;
+    printf("value after decrement: %d\n",*ptr);
+    ptr--;
+    printf("value after second decrement:%d\n",*ptr);
+    ptr=arr;
+    for(int i=0; i<5;i++){
+        printf("element %d:%d\n",i,*ptr);
+        ptr--;
+    }
+    return 0;
+}
+    
