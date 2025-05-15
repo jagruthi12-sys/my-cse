3d9a326b4b124f4db21b46d2206dbc5123edb6e5
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-15 09:35:36 +0530

    array number .c

---
+++ array number .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10],i,sum=0;
+    printf("enter 10 elements to array:");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("sum of all array elements is:%",sum);
+    return 0;
+}
+    
