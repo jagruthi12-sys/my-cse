6171152070be4419fb7a44b60c864adc97f1ed13
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-15 09:43:42 +0530

    Create array student marks .c

---
+++ array student marks .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k=1,mark[3][3];
+    printf("hi jk!\n");
+    printf("enter the marks of students:");
+    for(i=0;i<3;i++)
+      for(j=0;j<3;j++)
+          scanf("%d",&mark[i][j]);
+      for(i=0;i<3;i++)
+      {
+          printf("\nmarks of student %d is:",k);
+          for(j=0;j<3;j++)
+              printf("%4d",mark[i][j]);
+          k=k+1;
+      }
+      return 0;
+}
