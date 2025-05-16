0decd7e6d2140b803cff7e31ce2d0205daf8b7a2
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-16 12:31:34 +0530

    pointer reverse number .c

---
+++ pointer reverse number .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int arr[10]={1,2,3,4},n=4,i,temp,*start,*end;
+    start = arr;
+    end = arr +n -1;
+    while(start<end){
+         temp =*start;
+         *start = *end;
+         *end = temp;
+         start++;
+         end--;
+         }
+         for(i=0;i<n;i++){
+             printf("%d",arr[i]);
+         }
+    }
+    
