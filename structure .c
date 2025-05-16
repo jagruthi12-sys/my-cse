eae7bec4eddf97f291fa0a4e3da7ea36d944883d
jagruthi12-sys <jagruthi2006jagruthi2006@gmail.com>
2025-05-16 15:31:10 +0530

    structure .c

---
+++ structure .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    struct jagu {
+        char name[5];
+        int age;
+        float weight;
+    };
+    struct jagu j1;
+    printf("enter the name");
+    scanf("%s",&j1.name);
+    printf("enter the age");
+    scanf("%d",&j1.age);
+    printf("enter the weight");
+    scanf("%f",&j1.weight);
+    }
+    
